5da9c7873d0ee230f5d19a4cbcefaf669cd4c0b6
Tek233 <ab.ga881@gmail.com>
2026-02-13 20:32:53 +0100

    Linear Regression Implementation

--- LinearRegression/main.py
+++ LinearRegression/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 
@@ -9,11 +10,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-print(X_train.shape)
-print(y_train.shape)
-
-plt.scatter(X, y, marker="o", s=30)
-plt.show()
+# print(X_train.shape)
+# print(y_train.shape)
 
 
 class LinearRegression:
@@ -24,7 +22,37 @@ class LinearRegression:
         self.bias = None
 
     def fit(self, X, y):
-        pass
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X, self.weights) + self.bias
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+            self.bias -= self.lr * db
+            self.weights -= self.lr * dw
 
     def predict(self, X):
-        pass
+        y_predicted = np.dot(X, self.weights) + self.bias
+        return y_predicted
+
+
+def mse(y_true, y_predicted):
+    return np.mean((y_true - y_predicted) ** 2)
+
+
+regressor = LinearRegression(lr=0.01)
+regressor.fit(X_train, y_train)
+predicted = regressor.predict(X_test)
+
+mse_value = mse(y_test, predicted)
+print(mse_value)
+
+y_pred_line = regressor.predict(X)
+cmap = plt.get_cmap("viridis")
+fig = plt.figure(figsize=(8, 6))
+m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+plt.plot(X, y_pred_line, color="black", linewidth=2, label="Prediction")
+plt.show()
