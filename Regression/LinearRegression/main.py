76601c5a43cdd5f936f934055bbfa8a3420bca67
Tek233 <ab.ga881@gmail.com>
2026-02-20 18:14:03 +0100

    Refactor code create BaseRegression class

--- Regression/LinearRegression/main.py
+++ Regression/LinearRegression/main.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 
+from .. import BaseRegression
+
 X, y = make_regression(n_samples=100, n_features=1, noise=20, random_state=42)
 
 
@@ -10,32 +12,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# print(X_train.shape)
-# print(y_train.shape)
 
+class LinearRegression(BaseRegression):
+    def _approximation(self, X, w, b):
+        return np.dot(X, w) + b.bias
 
-class LinearRegression:
-    def __init__(self, lr=0.001, n_iters=1000):
-        self.lr = lr
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
-
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-
-        for _ in range(self.n_iters):
-            y_predicted = np.dot(X, self.weights) + self.bias
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
-            self.bias -= self.lr * db
-            self.weights -= self.lr * dw
-
-    def predict(self, X):
-        y_predicted = np.dot(X, self.weights) + self.bias
-        return y_predicted
+    def _predict(self, X, w, b):
+        return np.dot(X, w) + b
 
 
 def mse(y_true, y_predicted):
--- Regression/LogisticRegression/main.py
+++ Regression/LogisticRegression/main.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
+from .. import BaseRegression
+
 bc = datasets.load_breast_cancer()
 X, y = bc.data, bc.target
 
@@ -10,28 +12,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-class LogisticRegression:
-    def __init__(self, lr=0.001, n_iters=1000):
-        self.lr = lr
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
+class LogisticRegression(BaseRegression):
+    def _approximation(self, X, w, b):
+        linear_model = np.dot(X, w) + b.bias
+        return self._sigmoid(linear_model)
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-
-        for _ in range(self.n_iters):
-            linear_model = np.dot(X, self.weights) + self.bias
-            y_predicted = self._sigmoid(linear_model)
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
-            self.weights -= self.lr * dw
-            self.bias -= self.lr * db
-
-    def predict(self, X):
-        linear_model = np.dot(X, self.weights) + self.bias
+    def _predict(self, X, w, b):
+        linear_model = np.dot(X, w) + b
         y_predicted = self._sigmoid(linear_model)
         y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
         return y_predicted_cls
