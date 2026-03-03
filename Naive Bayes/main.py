75f932d2a6597d27be582677acb144154edb7a2f
Tek233 <ab.ga881@gmail.com>
2026-03-03 15:24:33 +0100

    NaiveBayes(Gaussian)

---
+++ Naive Bayes/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+X = np.array(
+    [
+        [5.1, 3.5, 1.4, 0.2],
+        [4.9, 3.0, 1.4, 0.2],
+        [7.0, 3.2, 4.7, 1.4],
+        [6.4, 3.2, 4.5, 1.5],
+        [6.3, 3.3, 6.0, 2.5],
+    ]
+)
+
+y = np.array([0, 0, 1, 1, 2])
+
+print(np.unique(y, return_counts=True)[1])
+
+
+class NaiveBayes:
+    def __init__(self):
+        self.means = {}
+        self.variances = {}
+        self.priors = {}
+
+    def fit(self, X, y):
+        for c in np.unique(y):
+            X_c = X[y == c]
+            self.priors[c] = X_c.shape[0] / X.shape[0]
+            self.means[c] = np.mean(X_c, axis=0)
+            self.variances[c] = np.var(X_c, axis=0) + 1e-9  # smoothing value
+        print(f"perior: {self.priors}")
+        print(f"mean: {self.means}")
+
+    def predict(self, x):
+        prob = {}
+        for c in self.priors.keys():
+            mean = self.means[c]
+            var = self.variances[c]
+            prior = self.priors[c]
+            dom = 1 / (np.sqrt(2 * np.pi * var))
+            num = np.exp((-((x - mean) ** 2)) / (2 * var))
+            prob_feature = num * dom
+            total = np.prod(prob_feature)
+            prob[c] = prior * total
+        print(f"\nPrrrr: {prob}\n")
+        return max(prob, key=prob.get)
+
+
+cls = NaiveBayes()
+cls.fit(X, y)
+print(f"prediction: {cls.predict([6.3, 3.3, 6.0, 2.5])}")
+print(f"prediction: {cls.predict([5.1, 4, 1, 0.7])}")
+print(f"prediction: {cls.predict([6.9, 3.0, 5.5, 4.5])}")
